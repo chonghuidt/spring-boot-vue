a2dd30b0fedb20f2f5d0a5cd82c7c1c7709f60f1
烟消云散 <1269653850@qq.com>
2019-11-29 09:47:17 +0800

    入门级的vue整合springboot

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+ <!--   <router-view/>-->
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+/*export default {
+  name: 'App'
+}*/
+import Login from '@/components/register'
+export default {
+  name: 'App',
+  components: {
+    'Login': Login
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+#app {
+ /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+  text-align: center;
+  margin-top:400px ;
+}
+</style>
